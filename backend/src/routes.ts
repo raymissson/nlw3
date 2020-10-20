0ed80269c690fc04f7a7aa842fa36bd3290223bb
raymissson <raymison1802@gmail.com>
2020-10-20 17:23:16 -0400

    backend completo

---
+++ backend/src/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
+
+import OrphanageController from './controllers/OrphanageController';
+
+const routes = Router();
+
+const upload = multer(uploadConfig);
+
+routes.post('/orphanages', upload.array('images'),OrphanageController.create);
+routes.get('/orphanages', OrphanageController.index);
+routes.get('/orphanages/:id', OrphanageController.show);
+
+
+
+export default routes;
